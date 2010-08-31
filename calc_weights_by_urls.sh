7fdc9b71bfe1caa28b723f71d1f6ec1dcbf78735
knevcher <knevcher@gmail.com>
2010-08-31 14:32:50 +0400

    -- added average request time -- sets of params replaced by PARAMS or IDS

--- calc_weights_by_urls.sh
+++ calc_weights_by_urls.sh
@@ -9,7 +9,15 @@ gawk -v parsing_interval=$1 '
   if(log_timestamp >= starting_timestamp)
   {
     split($12, url, "?");
-    gsub(/[0-9]+/, "ID", url[1]);
+
+
+    is_ids = match(url[1], /\/(([0-9]+)(,|))+/)
+    if(is_ids)
+      gsub(/\/(([0-9]+)(,|))+/, "/IDS", url[1]);
+
+    is_params = match(url[1], /\/([0-9a-zA-Z]+),(([0-9a-zA-Z]+)(,|))+/)
+    if(is_params && !is_ids)
+      gsub(/\/([0-9a-zA-Z]+),(([0-9a-zA-Z]+)(,|))+/, "/PARAMS", url[1]);
 
     current_url = url[1]
     status = $11
@@ -17,15 +25,15 @@ gawk -v parsing_interval=$1 '
     total_requests_weight += $4;
     total_requests_counter += 1;
 
-    urls_weights[status" "current_url] += $4;
-    requests_count[status" "current_url] += 1;
+    urls_weights[status" - "current_url] += $4;
+    requests_count[status" - "current_url] += 1;
 
     temp_max = $4;
-    if (temp_max > max_request_weight[status" "current_url]) max_request_weight[status" "current_url] = temp_max;
+    if (temp_max > max_request_weight[status" - "current_url]) max_request_weight[status" - "current_url] = temp_max;
 
     temp_min = $4;
-    if (min_request_weight[status" "current_url] == 0) min_request_weight[status" "current_url] = temp_min;
-    else if( min_request_weight[status" "current_url] > temp_min) min_request_weight[status" "current_url] = temp_min;
+    if (min_request_weight[status" - "current_url] == 0) min_request_weight[status" - "current_url] = temp_min;
+    else if( min_request_weight[status" - "current_url] > temp_min) min_request_weight[status" - "current_url] = temp_min;
 
     if(min_log_timestamp == 0)
       min_log_timestamp = log_timestamp
@@ -40,7 +48,8 @@ END{
     print " Total requests weight:"total_requests_weight;
     for(i in urls_weights)
     {
-      print urls_weights[i] ";" i ";" requests_count[i] ";" min_request_weight[i] ";" max_request_weight[i];
+      average_request_weight = (max_request_weight[i] + min_request_weight[2]) / 2;
+      print i ";" requests_count[i] ";" urls_weights[i] ";" min_request_weight[i] ";" max_request_weight[i] ";" ;
     }
   }
   else
